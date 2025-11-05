0d522253b8f0e0cd9651d8e3fa2be55124ec462c
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-11-05 22:11:32 +0600

    first commit

---
+++ Lesson1/student.js
@@ -0,0 +1,21 @@
+// module
+
+const getName = () =>{
+    return "Tanjila";
+}
+
+const getAge = () =>{
+    return "25";
+}
+
+const cgpa = 3.50;
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+
+module.exports ={
+    getName,
+    getAge,
+    cgpa
+}
